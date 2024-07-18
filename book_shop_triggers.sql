2bcd89351a894437e0bba5838e96ca4287957f10
Rodrigo Costa <rlcosta177@gmail.com>
2024-07-18 09:03:02 +0000

    Update book_shop_triggers.sql

--- book_shop_triggers.sql
+++ book_shop_triggers.sql
@@ -42,3 +42,142 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- -------- Null Handlers -------- --
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_autores`
+BEFORE INSERT ON `autores`
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "nome" cannot be null in autores table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_categorias`
+BEFORE INSERT ON `categorias`
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "nome" cannot be null in categorias table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_itens_venda`
+BEFORE INSERT ON `itens_venda`
+FOR EACH ROW
+BEGIN
+    IF NEW.quantidade IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "quantidade" cannot be null in itens_venda table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_livros`
+BEFORE INSERT ON `livros`
+FOR EACH ROW
+BEGIN
+    IF NEW.titulo IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "titulo" cannot be null in livros table';
+    END IF;
+    IF NEW.preco IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "preco" cannot be null in livros table';
+    END IF;
+    IF NEW.quantidade_em_stock IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "quantidade_em_stock" cannot be null in livros table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_clientes`
+BEFORE INSERT ON `clientes`
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "nome" cannot be null in clientes table';
+    END IF;
+    IF NEW.email IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "email" cannot be null in clientes table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_posicao`
+BEFORE INSERT ON `posicao`
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "nome" cannot be null in posicao table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_funcionarios`
+BEFORE INSERT ON `funcionarios`
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "nome" cannot be null in funcionarios table';
+    END IF;
+    IF NEW.salario IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "salario" cannot be null in funcionarios table';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER `before_insert_vendas`
+BEFORE INSERT ON `vendas`
+FOR EACH ROW
+BEGIN
+    IF NEW.data_venda IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The field "data_venda" cannot be null in vendas table';
+    END IF;
+END //
+
+DELIMITER ;
